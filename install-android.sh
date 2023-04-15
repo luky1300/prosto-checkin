a2bf5d2464bce4c4a7c7d385f88500746479ce80
luky1300 <jstrelnikova@gmail.com>
2023-04-15 12:53:04 -0400

    Bump version to be compiled with latest Android Studio

---
+++ install-android.sh
@@ -0,0 +1,8 @@
+mkdir -p android/app/src/main/assets
+npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+cd android
+./gradlew assembleDebug
+
+#rem adb shell pm uninstall com.prostocheckin
+adb install app/build/outputs/apk/debug/app-debug.apk
+cd ..
